c395ec9180bf0f02e4bf991a3aeff8b3307c092a
noemiNAV <noemiancari.na@gmail.com>
2019-04-11 22:33:47 -0400

    utilizamos un statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,10 @@ import 'package:english_words/english_words.dart';
 void main() {
   runApp(MyApp());
 } //
-
+//no persisten los datos con el StatelessWidget no permanecen los estados
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return MaterialApp(
       title: "bienvenido",
       home: Scaffold(
@@ -17,9 +16,24 @@ class MyApp extends StatelessWidget {
           title: Text('ORGANIZACIÓN DE EVENTOS'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
