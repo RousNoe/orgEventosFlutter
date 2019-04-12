e1787ba3a0b11b7caa405f76139a5525d46dd522
noemiNAV <noemiancari.na@gmail.com>
2019-04-12 19:39:03 -0400

    creacion de un drawer y logo eventos

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-} //
-//no persisten los datos con el StatelessWidget no permanecen los estados
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "bienvenido",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ORGANIZACIÓN DE EVENTOS'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      title: 'Organización de eventos',
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
+      home: Eventos(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return RandomWordsState();
-  }
-}
-
-class RandomWordsState extends State<RandomWords>{
+class Eventos extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      endDrawer: Drawer(),
+      appBar: AppBar(
+        title: Image.asset("assets/images/logo.jpg",height: 50.0,),
+      ),
+    );
   }
+
+}
