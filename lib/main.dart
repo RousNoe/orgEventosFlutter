1c46cbcdbbf58dada8fbb29ac295dea65fcb3f75
noemiNAV <noemiancari.na@gmail.com>
2019-04-12 19:30:02 -0400

    creacion de  Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ class Eventos extends StatelessWidget {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(12.0),
@@ -66,39 +67,36 @@ class Eventos extends StatelessWidget {
           ),
           Container(
             height: 150.0,
-            child: ListView(
+            child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                Padding(
+              itemBuilder: (context, position){
+                return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     width: 200.0,
                     height: 150.0,
                     color: Colors.orangeAccent[200],
+                    child: Text("PAQUETE $position+1"),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 200.0,
-                    height: 150.0,
-                    color: Colors.orangeAccent[200],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 200.0,
-                    height: 150.0,
-                    color: Colors.orangeAccent[200],
-                  ),
-                ),
+                );
+              },
+              ),
+            ),
+          DefaultTabController(
+            length: 3,
+            initialIndex: 1,
+            child: TabBar(
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: Colors.orangeAccent,
+              tabs:[
+                Tab(text: "Eventos",),
+                Tab(text: "Clientes",),
+                Tab(text: "Reuniones",),
               ],
             ),
-          )
-
+          ),
         ],
-      ),
-    );
+          )
+      );
   }
 }
