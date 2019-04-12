e572b25df757a56f5a46312de803e6c2d00e75c2
noemiNAV <noemiancari.na@gmail.com>
2019-04-11 21:57:17 -0400

    solo texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main(){
-
   runApp(
-    MaterialApp (
-      title:'MI Aplicacion ',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Organizacion de Eventos'),
-        ),
-        body: Center(
-          child: Text('Hola mi mundo'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: (){},
-        ),
-
+    Center(
+      child: Text(
+        'hola mundo por Noemi :-)',
+        textDirection: TextDirection.ltr,
       ),
-
-    )
-    );
+    ),
+  );
 }
