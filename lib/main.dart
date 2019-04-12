65c460d7008ae5bfa9f60ea01fc87a262aaa4afc
noemiNAV <noemiancari.na@gmail.com>
2019-04-11 22:16:31 -0400

    añadiendo un paquete al proyecto- un generador de palabras

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main(){
-  runApp(
-    Center(
-      child: Text(
-        'hola mundo por Noemi :-)',
-        textDirection: TextDirection.ltr,
+
+void main() {
+  runApp(MyApp());
+} //
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return MaterialApp(
+      title: "bienvenido",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ORGANIZACIÓN DE EVENTOS'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
