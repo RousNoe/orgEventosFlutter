cc4be4743eda458e39680c6eae6577d92b4d5c22
noemiNAV <noemiancari.na@gmail.com>
2019-04-12 19:32:43 -0400

    contenedor en list

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,15 +15,90 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class Eventos extends StatelessWidget{
+class Eventos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       endDrawer: Drawer(),
       appBar: AppBar(
-        title: Image.asset("assets/images/logo.jpg",height: 50.0,),
+        title: Image.asset(
+          "assets/images/logo.jpg",
+          height: 60.0,
+        ),
+        actions: <Widget>[
+          InkWell(
+            onTap: () {
+              print("Boton - Anotaciones");
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Icon(Icons.note_add),
+            ),
+          ),
+          InkWell(
+              onTap: () {
+                print("Boton - busqueda");
+              },
+              child: Icon(Icons.search)),
+          InkWell(
+            onTap: () {
+              print("Boton - dehaze");
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Icon(Icons.dehaze),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "Visualiza aqui tus Eventos",
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w600,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200.0,
+                    height: 150.0,
+                    color: Colors.orangeAccent[200],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200.0,
+                    height: 150.0,
+                    color: Colors.orangeAccent[200],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200.0,
+                    height: 150.0,
+                    color: Colors.orangeAccent[200],
+                  ),
+                ),
+              ],
+            ),
+          )
+
+        ],
       ),
     );
   }
-
 }
